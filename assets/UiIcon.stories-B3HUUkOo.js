d810e14b9053cf60488e04080b0ebff44ff9dcda
colmmcsharry <colmmcsharry@users.noreply.github.com>
2025-02-10 12:54:10 +0000

    deploy: cc43f4e70233fd326a529ea5bbab044c4937931a

---
+++ assets/UiIcon.stories-B3HUUkOo.js
@@ -0,0 +1,5 @@
+import{U as t,E as o}from"./UiIcon-CyY_hcMz.js";import"./jsx-runtime-CLpGMVip.js";import"./index-B-SYruCi.js";import"./index-JoFrltcn.js";import"./index-CXyNTX1A.js";const l={title:"Components/UiIcon",component:t,argTypes:{size:{control:{type:"select"},options:Object.values(o),description:"Icon Size"}},args:{size:o.LG}},e={args:{name:["far","face-smile"]}};var r,s,a;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    name: ["far", "face-smile"]
+  }
+}`,...(a=(s=e.parameters)==null?void 0:s.docs)==null?void 0:a.source}}};const f=["Default"];export{e as Default,f as __namedExportsOrder,l as default};
