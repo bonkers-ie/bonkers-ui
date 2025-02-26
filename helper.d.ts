d562990da5e42e44d31a751639afd43d76746a6c
AndrewFedotiuk <AndrewFedotiuk@users.noreply.github.com>
2025-02-26 17:27:32 +0000

    deploy: a7aabc7f40efbef72863ee755aacce547095fe6a

--- helper.d.ts
+++ helper.d.ts
@@ -1,1 +1,32 @@
+export type TColor = {
+    name: string;
+    value: string;
+};
+export type TGroupedColor = {
+    groupTitle: string;
+    groupColors: {
+        shade: string;
+        value: string;
+        name: string;
+    }[];
+};
 export declare const getCssVariableValue: (variable: string) => string;
+export declare const getBaseVariables: () => {
+    name: string;
+    value: string;
+}[];
+export declare const getThemeTokens: () => {
+    fontSizes: {
+        name: string;
+        value: string;
+    }[];
+    spacings: {
+        name: string;
+        value: string;
+    }[];
+    groupedColors: TGroupedColor[];
+    baseColors: {
+        name: string;
+        value: string;
+    }[];
+};
