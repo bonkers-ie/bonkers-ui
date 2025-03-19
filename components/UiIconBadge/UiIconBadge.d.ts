364af696f527ab7da18b707496e3b4cb18e3042b
colmmcsharry <colmmcsharry@users.noreply.github.com>
2025-03-19 14:55:55 +0000

    deploy: 44c361de397fc9772daf96b6694688d22219da58

---
+++ components/UiIconBadge/UiIconBadge.d.ts
@@ -0,0 +1,3 @@
+import { default as React } from '../../../node_modules/react';
+import { IUiIconBadgeProps } from './_types';
+export declare const UiIconBadge: React.FC<IUiIconBadgeProps>;
---
+++ components/UiIconBadge/_types.d.ts
@@ -0,0 +1,14 @@
+import { ESize } from '../../_types/sizing';
+export type BadgeSize = ESize.SM | ESize.MD | ESize.LG | ESize.XL;
+export declare enum EIconBadgeKind {
+    PRIMARY = "primary",
+    LIGHT = "light",
+    DARK = "dark",
+    WARNING = "warning",
+    ERROR = "error"
+}
+export interface IUiIconBadgeProps {
+    size?: BadgeSize;
+    kind?: EIconBadgeKind;
+    children: React.ReactNode;
+}
