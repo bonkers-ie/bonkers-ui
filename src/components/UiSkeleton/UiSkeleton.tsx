7ba8d7424f16e03dfb8055a61961859e419cfc9d
rebeccaOberto <109217684+rebeccaOberto@users.noreply.github.com>
2025-03-25 11:40:06 +0000

    #AA-132 (update): UiSkeleton mobile

--- src/components/UiSkeleton/UiSkeleton.tsx
+++ src/components/UiSkeleton/UiSkeleton.tsx
@@ -205,6 +205,243 @@ export const UiSkeleton: React.FC<UiSkeletonProps> = ({
 
 			) }
 
+			{ kind === ESkeletonKind.RESULT_CARD_MOBILE && (
+				<div
+					className={ cx(
+						"ui-skeleton-card-mobile",
+						"animate-pulse",
+						"border-2",
+						"border-secondary-alt-300",
+						"rounded-2xl",
+						"grid",
+						"max-w-[382px]",
+						"p-sm"
+					) }>
+					<div
+						className={ cx(
+							"ui-skeleton-card__content"
+
+						) }
+					>
+						<div className={ cx("flex", "grow", "gap-sm") }>
+							<div
+								className={ cx(
+									"ui-skeleton-card__content",
+									"bg-secondary-alt-400",
+									"h-xxxl",
+									"rounded-sm",
+									"w-xxxl"
+								) }
+							/>
+							<div className={ cx("flex","flex-col", "grow") }>
+								<div
+									className={ cx(
+										"ui-skeleton-card__content",
+										"bg-secondary-alt-400",
+										"h-md",
+										"mb-sm",
+										"rounded-sm",
+										"w-4/5"
+									) }
+								/>
+								<div
+									className={ cx(
+										"ui-skeleton-card__content",
+										"bg-secondary-alt-400",
+										"h-md",
+										"mb-sm",
+										"rounded-sm",
+										"w-2/3"
+									) }
+								/>
+							</div>
+						</div>
+					</div>
+					<div className={ cx(
+						"flex",
+						"grow",
+						"gap-xxs",
+						"justify-center",
+						"border-t",
+						"p-sm",
+						"border-secondary-alt-300") }>
+
+						<div
+							className={ cx(
+								"ui-skeleton-card__content",
+								"bg-secondary-alt-500",
+								"h-xxxxl",
+								"rounded-full",
+								"w-xxxxl"
+
+							) }
+						/>
+					</div>
+					<div className={ cx(
+						"grid",
+						"grid-cols-3",
+
+					) } >
+						{ Array.from({
+							length: 3
+						}).map((_, index)=> (
+							<div key={ index } className={ cx(
+								"border",
+								"border-l-0",
+								"border-secondary-alt-300",
+								"py-xxs",
+								index === 2 ? "border-r-0" : ""
+
+							) } >
+								<div className={ cx(
+									"place-items-center"
+
+								) }>
+									{ ["w-2/3", "w-2/4"].map((widthClass, index) => {
+										const bgClass = index === 0 ? "bg-secondary-alt-300" : "bg-secondary-alt-400";
+
+										return (
+											<div
+												key={ index }
+												className={ cx(
+													"bg-secondary-alt-300",
+													"mb-xxxs",
+													"rounded-sm",
+													"h-[23px]",
+													bgClass,
+													widthClass,
+
+												) }
+											/>
+										);
+									}) }
+								</div>
+							</div>
+						)) }
+
+					</div>
+
+					<div className={ cx(
+						"flex",
+						"gap-sm",
+						"my-xxs"
+					) }>
+						{ ["w-2/6", "w-2/6"].map((widthClass, index) => {
+							const bgClass = index === 0 ? "bg-secondary-alt-300" : "bg-secondary-alt-300";
+
+							return (
+								<div
+									key={ index }
+									className={ cx(widthClass, "h-md", bgClass, "rounded-sm") }
+								/>
+							);
+						}) }
+					</div>
+
+					<div className={ cx(
+						"flex",
+						"justify-between"
+					) }>
+
+						<div className={ cx(
+							"flex",
+							"flex-col",
+							"gap-xxxs",
+							"w-full"
+
+						) }>
+
+							{ ["w-3/5", "w-2/5", "w-3/6","w-3/5"].map((widthClass, index) => {
+								const bgClass = index === 3 ? "bg-secondary-alt-400" : "bg-secondary-alt-300";
+								const heightClass = index === 2 ? "h-[23px]" : index === 3 ? "h-sm" : "h-[20px]";
+								return (
+
+									<div
+										key={ index }
+										className={ cx(
+											"rounded-sm",
+											widthClass,
+											heightClass,
+											bgClass
+
+										) }
+									/>
+								);
+							}) }
+						</div>
+
+						<div className={ cx(
+							"flex",
+							"flex-col",
+							"gap-xxxs",
+							"w-full",
+							"items-end"
+						) }>
+
+							{ ["w-3/6", "w-2/5", "w-3/5"].map((widthClass, index)=> {
+								const bgClass = index === 1 ? "bg-secondary-alt-300" : "bg-secondary-alt-400";
+								const roundClass = index === 2 ? "rounded-full" : "rounded-sm";
+								const heightClass = index === 1 ? "h-[20px]" : "h-md";
+								const marginsClass = index === 0  ? "mt-xxs" : index === 2 ? "mt-xxs" : "mt-0";
+								return (
+
+									<div
+										key={ index }
+										className={ cx(
+											roundClass,
+											widthClass,
+											heightClass,
+											bgClass,
+											marginsClass,
+
+										) }
+									/>
+								);
+							}) }
+						</div>
+					</div>
+
+					<div className={ cx(
+						"flex",
+						"h-md",
+						"w-full",
+						"rounded-sm",
+						"bg-secondary-alt-400",
+						"mt-sm"
+					) } />
+
+					<div className={ cx(
+						"flex",
+						"h-xxl",
+						"w-full",
+						"rounded-lg",
+						"bg-secondary-alt-500",
+						"mt-xxs"
+					) } />
+
+					<div className={ cx(
+						"flex",
+						"border-b",
+						"border-secondary-alt-400",
+						"mt-sm",
+						"-mx-sm"
+
+					) }
+					/>
+
+					<div className={ cx(
+						"flex",
+						"h-sm",
+						"justify-center",
+						"bg-secondary-alt-300",
+						"rounded-sm",
+						"mt-sm"
+					) }
+					/>
+
+				</div>
+			) }
+
 		</div>
 	);
 };
