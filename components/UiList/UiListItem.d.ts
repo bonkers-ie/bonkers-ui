1dc2a080bb57f05058ebbc69d2811885892a5202
AndrewFedotiuk <AndrewFedotiuk@users.noreply.github.com>
2025-04-02 16:04:01 +0000

    deploy: 4173639220f49e5ba9fa8de47bd669b9826d6c21

--- components/UiList/UiListItem.d.ts
+++ components/UiList/UiListItem.d.ts
@@ -1,8 +1,10 @@
 import { default as React } from '../../../node_modules/react';
+import { EListItemSpacing } from './_types';
 interface IUiListItemProps {
     children: React.ReactNode;
     className?: string;
     prefix?: React.ReactNode;
+    space?: EListItemSpacing;
 }
 export declare const UiListItem: React.FC<IUiListItemProps>;
 export {};
