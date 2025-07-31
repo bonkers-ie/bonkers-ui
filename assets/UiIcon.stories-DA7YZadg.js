7434a306528f06f5d41fa97f8651a870f59968ea
selucean0 <selucean0@users.noreply.github.com>
2025-07-31 11:57:19 +0000

    deploy: e215aff653e83a56589edadd6be19ffe0c51b261

---
+++ assets/UiIcon.stories-DA7YZadg.js
@@ -0,0 +1,5 @@
+import{E as o,U as t}from"./UiIcon-B6qIwEMh.js";import"./jsx-runtime-D_zvdyIk.js";import"./iframe-BwznxUV6.js";import"./index-CuoXRDjT.js";const p={title:"Components/UiIcon",component:t,argTypes:{size:{control:{type:"select"},options:Object.values(o),description:"Icon Size"}},args:{size:o.LG}},e={args:{name:["far","face-smile"]}};var r,s,a;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    name: ["far", "face-smile"]
+  }
+}`,...(a=(s=e.parameters)==null?void 0:s.docs)==null?void 0:a.source}}};const l=["Default"];export{e as Default,l as __namedExportsOrder,p as default};
--- assets/UiPills.stories-BXPgcF7C.js
+++
@@ -1,5 +0,0 @@
-import{j as s}from"./jsx-runtime-D_zvdyIk.js";import{E as r,a as n,U as l}from"./UiPills-D8FtgpZ9.js";import"./iframe-BJDpL1rA.js";import{U as c,E as d}from"./UiIcon-KnqHi_77.js";import"./index-3PPjqXSQ.js";import"./UiTypography-4ewrWduN.js";const S={title:"Components/UiPills",component:l,argTypes:{size:{control:{type:"select"},options:Object.values(r),description:"Pills Size"},kind:{control:{type:"select"},options:Object.values(n),description:"Pills Kind"},children:{control:{type:"text"},description:"Pills Children"}},args:{kind:n.PRIMARY,size:r.SMALL,children:"Pills"}},e={render:i=>s.jsx(l,{kind:i.kind,size:i.size,icon:s.jsx(c,{name:["far","face-smile"],size:d.XS}),children:i.children})};var o,t,a;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  render: args => <UiPills kind={args.kind} size={args.size} icon={<UiIcon name={["far", "face-smile"]} size={ESize.XS} />}>
-            {args.children}
-        </UiPills>
-}`,...(a=(t=e.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};const U=["Primary"];export{e as Primary,U as __namedExportsOrder,S as default};
