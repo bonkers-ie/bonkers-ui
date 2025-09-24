368981485f14d2dadcc83104e96b76e62bfec262
selucean0 <selucean0@users.noreply.github.com>
2025-09-24 11:59:26 +0000

    deploy: 5ae8b84aba86e64bf28aef4c0bcc0ada67149268

--- assets/UiIcon.stories-DZ4DrZh0.js
+++
@@ -1,5 +0,0 @@
-import{E as o,U as r}from"./UiIcon-ChLMJORw.js";import"./jsx-runtime-D_zvdyIk.js";import"./iframe-BV9BvOuM.js";import"./index-DRx6VtWy.js";const c={title:"Components/UiIcon",component:r,argTypes:{size:{control:{type:"select"},options:Object.values(o),description:"Icon Size"}},args:{size:o.LG}},e={args:{name:["far","face-smile"]}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {
-    name: ["far", "face-smile"]
-  }
-}`,...e.parameters?.docs?.source}}};const i=["Default"];export{e as Default,i as __namedExportsOrder,c as default};
