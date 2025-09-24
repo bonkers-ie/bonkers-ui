c87ad56ccf920228757e296b1d50cb8a647394a1
AndrewFedotiuk <AndrewFedotiuk@users.noreply.github.com>
2025-09-24 13:19:06 +0000

    deploy: 5f718971fa9445c720c0103683a86f3d0759d3af

---
+++ assets/UiIcon.stories-D_EUBf5b.js
@@ -0,0 +1,5 @@
+import{E as o,U as r}from"./UiIcon-ja_e6YjS.js";import"./jsx-runtime-D_zvdyIk.js";import"./iframe-GEoEkiyP.js";import"./preload-helper-PPVm8Dsz.js";import"./index-CB7PLhcH.js";const i={title:"Components/UiIcon",component:r,argTypes:{size:{control:{type:"select"},options:Object.values(o),description:"Icon Size"}},args:{size:o.LG}},e={args:{name:["far","face-smile"]}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    name: ["far", "face-smile"]
+  }
+}`,...e.parameters?.docs?.source}}};const m=["Default"];export{e as Default,m as __namedExportsOrder,i as default};
--- assets/UiIcon.stories-_aU73qJ1.js
+++
@@ -1,5 +0,0 @@
-import{E as o,U as r}from"./UiIcon-BD3uLNGB.js";import"./jsx-runtime-D_zvdyIk.js";import"./iframe-D-_bU5Uf.js";import"./index-DBazurmw.js";const c={title:"Components/UiIcon",component:r,argTypes:{size:{control:{type:"select"},options:Object.values(o),description:"Icon Size"}},args:{size:o.LG}},e={args:{name:["far","face-smile"]}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {
-    name: ["far", "face-smile"]
-  }
-}`,...e.parameters?.docs?.source}}};const i=["Default"];export{e as Default,i as __namedExportsOrder,c as default};
