c7f3f82c143af9407a3f375775d1bfc46f618fde
AndrewFedotiuk <AndrewFedotiuk@users.noreply.github.com>
2025-12-03 15:18:20 +0000

    deploy: be6c91a41f2c13312dcf7fc85856c3e9f1e298ef

---
+++ assets/UiRadioFancy.stories-CyNnv_Zy.js
@@ -0,0 +1,47 @@
+import{j as a}from"./jsx-runtime-u17CrQMm.js";import{r as p}from"./iframe-DSYpQr-U.js";import{c}from"./index-By4jBET3.js";import{U as y,E as v}from"./UiIcon-Y7uQtzDw.js";import"./preload-helper-PPVm8Dsz.js";var s=(e=>(e.DEFAULT="default",e.COMPACT="compact",e))(s||{});const f={[s.DEFAULT]:"items-center",[s.COMPACT]:"items-start flex-col"},o=({children:e,icon:r,disabled:n,subHeader:d,active:i,radioType:u=s.DEFAULT,value:m,onChange:g,className:h})=>a.jsxs("label",{className:c("ui-radio-fancy","relative",{"pointer-events-none":n},h),children:[a.jsx("input",{className:"group peer absolute appearance-none",type:"radio",checked:i,onChange:()=>g(m)}),a.jsxs("div",{className:c("box-border","cursor-pointer","rounded-lg","border","bg-white","p-sm","hover:border-secondary-alt-700","peer-focus:outline","peer-focus:outline-4","peer-focus:outline-primary-300","peer-checked:shadow-border-primary-600","peer-checked:border-0","active:peer-checked:bg-secondary-alt-200","hover:peer-checked:shadow-border-primary-700","flex","gap-sm","text-sm",i?"peer-focus:outline-offset-2":"peer-focus:outline-offset-0",n?"peer-checked:shadow-border-primary-disabled border-secondary-alt-400 text-secondary-alt-500":"border-secondary-alt-600",f[u]),children:[a.jsx("div",{className:c("relative","grid","size-fit","rounded-full","place-items-center","text-white","p-xxs",{"bg-primary-300":n,"bg-primary":!n}),children:r?a.jsx(y,{name:r,size:v.SM}):null}),a.jsxs("div",{className:"flex flex-col",children:[e,a.jsx("div",{className:"text-xs font-normal text-secondary-alt-400",children:d})]})]})]});o.__docgenInfo={description:"",methods:[],displayName:"UiRadioFancy",props:{children:{required:!0,tsType:{name:"ReactReactNode",raw:"React.ReactNode"},description:""},icon:{required:!1,tsType:{name:"IconProp"},description:""},disabled:{required:!1,tsType:{name:"boolean"},description:""},active:{required:!1,tsType:{name:"boolean"},description:""},subHeader:{required:!1,tsType:{name:"string"},description:""},className:{required:!1,tsType:{name:"string"},description:""},radioType:{required:!1,tsType:{name:"ERadioTypes"},description:"",defaultValue:{value:"ERadioTypes.DEFAULT",computed:!0}},value:{required:!0,tsType:{name:"string"},description:""},onChange:{required:!0,tsType:{name:"signature",type:"function",raw:"(value: string) => void",signature:{arguments:[{type:{name:"string"},name:"value"}],return:{name:"void"}}},description:""}}};const T={title:"Components/UiRadioFancy",component:o,argTypes:{children:{control:{type:"text"},description:"RadioFancy Children"},disabled:{control:{type:"boolean"},description:"Radio disabled"},radioType:{control:{type:"select"},options:Object.values(s),description:" radio type "}},args:{children:"Title",disabled:!1,radioType:s.DEFAULT,onChange:e=>console.log(e),active:!1,value:"value"}},l={render:e=>{const[r,n]=p.useState("value1"),d=i=>{n(i),e.onChange&&e.onChange(i)};return a.jsxs("div",{className:"grid grid-rows-2 gap-sm",children:[a.jsx(o,{disabled:e.disabled,icon:["far","face-smile"],radioType:e.radioType,value:"value1",onChange:d,active:r==="value1",children:e.children}),a.jsx(o,{disabled:e.disabled,icon:["far","face-smile"],radioType:e.radioType,value:"value2",onChange:d,active:r==="value2",children:e.children})]})}},t={args:{...T.args,radioType:s.COMPACT},render:e=>{const[r,n]=p.useState("value1"),d=i=>{n(i),e.onChange&&e.onChange(i)};return a.jsxs("div",{className:e.radioType===s.COMPACT?"grid w-[350px] grid-cols-2 gap-sm":"grid grid-rows-2 gap-sm",children:[a.jsx(o,{disabled:e.disabled,icon:["far","face-smile"],radioType:e.radioType,value:"value1",onChange:d,active:r==="value1",children:e.children}),a.jsx(o,{disabled:e.disabled,icon:["far","face-smile"],radioType:e.radioType,value:"value2",onChange:d,active:r==="value2",children:e.children})]})}};l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  render: args => {
+    const [selectedValue, setSelectedValue] = useState<string>("value1");
+    const handleChange = (value: string) => {
+      setSelectedValue(value);
+      if (args.onChange) {
+        args.onChange(value);
+      }
+    };
+    return <div className="grid grid-rows-2 gap-sm">
+                <UiRadioFancy disabled={args.disabled} icon={["far", "face-smile"]} radioType={args.radioType} value="value1" onChange={handleChange} active={selectedValue === "value1"}>
+                    {args.children}
+
+                </UiRadioFancy>
+
+                <UiRadioFancy disabled={args.disabled} icon={["far", "face-smile"]} radioType={args.radioType} value="value2" onChange={handleChange} active={selectedValue === "value2"}>
+                    {args.children}
+
+                </UiRadioFancy>
+            </div>;
+  }
+}`,...l.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    ...meta.args,
+    radioType: ERadioTypes.COMPACT
+  },
+  render: args => {
+    const [selectedValue, setSelectedValue] = useState<string>("value1");
+    const handleChange = (value: string) => {
+      setSelectedValue(value);
+      if (args.onChange) {
+        args.onChange(value);
+      }
+    };
+    return <div className={args.radioType === ERadioTypes.COMPACT ? "grid w-[350px] grid-cols-2 gap-sm" : "grid grid-rows-2 gap-sm"}>
+                <UiRadioFancy disabled={args.disabled} icon={["far", "face-smile"]} radioType={args.radioType} value="value1" onChange={handleChange} active={selectedValue === "value1"}>
+                    {args.children}
+
+                </UiRadioFancy>
+
+                <UiRadioFancy disabled={args.disabled} icon={["far", "face-smile"]} radioType={args.radioType} value="value2" onChange={handleChange} active={selectedValue === "value2"}>
+                    {args.children}
+
+                </UiRadioFancy>
+            </div>;
+  }
+}`,...t.parameters?.docs?.source}}};const F=["Primary","Variant"];export{l as Primary,t as Variant,F as __namedExportsOrder,T as default};
---
+++ assets/UiSnackbar.stories-n-eMb-n8.js
@@ -0,0 +1,29 @@
+import{j as t}from"./jsx-runtime-u17CrQMm.js";import{r as i}from"./iframe-DSYpQr-U.js";import{c as M}from"./index-By4jBET3.js";import{U as p,E as f}from"./UiIcon-Y7uQtzDw.js";import{U as O}from"./UiTypography-DPnFIvJ6.js";import"./preload-helper-PPVm8Dsz.js";var n=(e=>(e.SECONDARY="secondary",e.DEFAULT="default",e.PRIMARY="primary",e.WARNING="warning",e.ERROR="error",e))(n||{}),o=(e=>(e[e.SHORT=5e3]="SHORT",e[e.LONG=1e4]="LONG",e[e.INFINITE=1/0]="INFINITE",e))(o||{});const D={[n.DEFAULT]:`
+		bg-white
+		text-secondary-700
+
+	`,[n.PRIMARY]:`
+		bg-primary-alt-700
+		text-white
+	`,[n.WARNING]:`
+		bg-warning-600
+		text-white
+	`,[n.ERROR]:`
+		bg-error
+		text-white
+	`,[n.SECONDARY]:`
+		bg-secondary
+		text-white
+	`},c=({id:e,kind:R=n.SECONDARY,preIcon:N,postIcon:g,title:E,children:x,className:b,duration:a=o.INFINITE,pauseOnHover:y=!0,onClickAction:S,onDismissAction:j,isDismissed:I})=>{const[k,A]=i.useState(a),[h,T]=i.useState(!1),r=i.useRef(null),d=i.useRef(0),w=i.useRef(Date.now()),l=i.useRef(null);i.useEffect(()=>{if(!I||!l.current)return;const s=()=>{j?.(e),r.current&&clearInterval(r.current)};return l.current.addEventListener("animationend",s),()=>{l.current?.removeEventListener("animationend",s)}},[I]),i.useEffect(()=>{if(a===o.INFINITE)return;const s=()=>{const m=Date.now()-w.current,v=Math.max(0,a-m);A(v),v<=0&&(S?.(),r.current&&clearInterval(r.current))};if(h)r.current&&clearInterval(r.current),d.current=Date.now();else{if(d.current){const m=Date.now()-d.current;w.current+=m,d.current=0}s(),r.current=setInterval(s,100)}return()=>{r.current&&clearInterval(r.current)}},[h,a]);const U=()=>{y&&T(!0)},q=()=>{y&&T(!1)};return t.jsxs("div",{className:M("ui-snackbar","relative","overflow-clip","flex","w-full","rounded-lg","items-center","justify-between","shadow-md","items-center","px-sm","py-xs","flex-col","gap-y-xxxs","text-sm",D[R],b),ref:l,onMouseEnter:U,onMouseLeave:q,children:[t.jsxs("div",{className:"flex w-full justify-between",children:[t.jsxs("div",{className:"flex flex-row items-center gap-xxs",children:[N||null,E]}),g||null]}),x?t.jsx("div",{style:{color:R===n.DEFAULT?"black":"white",fontWeight:"normal"},className:"w-full",children:x}):null,t.jsx("div",{className:"absolute bottom-0 left-0 h-xxs bg-black opacity-30 bg-blend-color-burn",style:{width:a===o.INFINITE?"0%":`${k/a*100}%`,transition:"width 0.1s linear"}})]})};c.__docgenInfo={description:"",methods:[],displayName:"UiSnackbar",props:{id:{required:!0,tsType:{name:"string"},description:""},kind:{required:!1,tsType:{name:"ESnackbarTypes"},description:"",defaultValue:{value:"ESnackbarTypes.SECONDARY",computed:!0}},preIcon:{required:!1,tsType:{name:"ReactReactNode",raw:"React.ReactNode"},description:""},postIcon:{required:!1,tsType:{name:"ReactReactNode",raw:"React.ReactNode"},description:""},title:{required:!0,tsType:{name:"ReactReactNode",raw:"React.ReactNode"},description:""},children:{required:!1,tsType:{name:"ReactReactNode",raw:"React.ReactNode"},description:""},className:{required:!1,tsType:{name:"string"},description:""},duration:{required:!1,tsType:{name:"ESnackbarDuration"},description:"",defaultValue:{value:"ESnackbarDuration.INFINITE",computed:!0}},pauseOnHover:{required:!1,tsType:{name:"boolean"},description:"",defaultValue:{value:"true",computed:!1}},isDismissed:{required:!1,tsType:{name:"boolean"},description:""},onClickAction:{required:!1,tsType:{name:"signature",type:"function",raw:"() => void",signature:{arguments:[],return:{name:"void"}}},description:""},onDismissAction:{required:!1,tsType:{name:"signature",type:"function",raw:"(id: string) => void",signature:{arguments:[{type:{name:"string"},name:"id"}],return:{name:"void"}}},description:""}}};const G={title:"Components/UiSnackbar",component:c,argTypes:{kind:{control:{type:"select"},options:Object.values(n),description:"Snackbar Color"},title:{control:{type:"text",description:"text or title"}},children:{control:{type:"text",description:"text or child element/s"}},preIcon:{control:{type:"text",description:"ReactNode element that is positioned at the start of the snackbar"}},postIcon:{control:{type:"text",description:"ReactNode element that is positioned at the end of the snackbar"}}},args:{id:"unique-id-1",kind:n.SECONDARY,title:"Some Text",postIcon:t.jsx("p",{className:"underline",children:"Text"})}},u={render:e=>t.jsxs("div",{className:"grid gap-md",children:[t.jsx(c,{...e}),t.jsx(c,{id:"unique-id-2",kind:n.PRIMARY,postIcon:t.jsx(p,{name:["far","face-angry"],size:f.SM}),title:"Primary Notification"}),t.jsx(c,{id:"unique-id-3",kind:n.WARNING,preIcon:t.jsx(p,{name:["far","face-smile"],size:f.SM}),postIcon:t.jsx(p,{name:["far","face-angry"],size:f.SM}),title:"Warning Notification with content",duration:o.LONG,children:t.jsx(O,{lineHeight:!0,children:"This is a warning notification with some content."})})]})};u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  render: args => {
+    return <div className="grid gap-md">
+            <UiSnackbar {...args} />
+            <UiSnackbar id="unique-id-2" kind={ESnackbarTypes.PRIMARY} postIcon={<UiIcon name={["far", "face-angry"]} size={ESize.SM} />} title="Primary Notification" />
+            <UiSnackbar id="unique-id-3" kind={ESnackbarTypes.WARNING} preIcon={<UiIcon name={["far", "face-smile"]} size={ESize.SM} />} postIcon={<UiIcon name={["far", "face-angry"]} size={ESize.SM} />} title="Warning Notification with content" duration={ESnackbarDuration.LONG}>
+                <UiTypography lineHeight>
+                    This is a warning notification with some content.
+                </UiTypography>
+            </UiSnackbar>
+        </div>;
+  }
+}`,...u.parameters?.docs?.source}}};const W=["Primary"];export{u as Primary,W as __namedExportsOrder,G as default};
